199cd21b08c3d307454ae312a7e52b0540e6fba2
jfreissmann <j.freissmann@web.de>
2024-04-03 15:28:40 +0200

    Change model data structure.

--- src/heatpumps/variables.py
+++ src/heatpumps/variables.py
@@ -1,78 +1,188 @@
-# Important variables for the heat pump dashboard
+# %% Important variables for the heat pump dashboard
 
-# hp_models = {
-#     'cascade_2ihx': HeatPumpCascade2IHX.HeatPumpCascade2IHX,
-#     'cascade': HeatPumpCascade.HeatPumpCascade,
-#     'econ_closed_ihx': HeatPumpEconIHX.HeatPumpEconIHX,
-#     'econ_closed': HeatPumpEcon.HeatPumpEcon,
-#     'econ_open_ihx': HeatPumpEconIHX.HeatPumpEconIHX,
-#     'econ_open': HeatPumpEcon.HeatPumpEcon,
-#     'flash': HeatPumpFlash.HeatPumpFlash,
-#     'ic': HeatPumpIC.HeatPumpIC,
-#     'ihx_econ_closed': HeatPumpIHXEcon.HeatPumpIHXEcon,
-#     'ihx_econ_open': HeatPumpIHXEcon.HeatPumpIHXEcon,
-#     'ihx': HeatPumpIHX.HeatPumpIHX,
-#     'pc_econ_closed_2ihx': HeatPumpPC2IHX.HeatPumpPC2IHX,
-#     'pc_econ_closed_ihx': HeatPumpPCIHX.HeatPumpPCIHX,
-#     'pc_econ_closed': HeatPumpPC.HeatPumpPC,
-#     'pc_econ_open_2ihx': HeatPumpPC2IHX.HeatPumpPC2IHX,
-#     'pc_econ_open_ihx': HeatPumpPCIHX.HeatPumpPCIHX,
-#     'pc_econ_open': HeatPumpPC.HeatPumpPC,
-#     'simple': HeatPumpSimple.HeatPumpSimple
-#     }
+# %% Model parameters
+base_topologies = (
+    'Einfacher Kreis',
+    'Zwischenkühlung',
+    'Economizer',
+    'Flashtank',
+    'Kaskadierter Kreis'
+    )
 
-hp_topologies = {
-    'Einfacher Kreis': {
-        'simple': {'nr_int_heatex': 0},
-        'ihx': {'nr_int_heatex': 1}
+hp_models = {
+    'simple': {
+        'base_topology': 'Einfacher Kreis',
+        'display_name': 'Allgemein',
+        'nr_ihx': 0,
+        'econ_type': None,
+        'comp_var': None,
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
         },
-    'Zwischenkühlung': {
-        'ic': {'nr_int_heatex': 0}
+    'ihx': {
+        'base_topology': 'Einfacher Kreis',
+        'display_name': 'Interne WÜT',
+        'nr_ihx': 1,
+        'econ_type': None,
+        'comp_var': None,
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
         },
-    'Economizer': {
-        'econ_closed': {
-            'nr_int_heatex': 0, 'econ_type': 'closed', 'comp_var': 'series'
-            },
-        'econ_closed_ihx': {
-            'nr_int_heatex': 1, 'econ_type': 'closed', 'comp_var': 'series'
-            },
-        'ihx_econ_closed': {
-            'nr_int_heatex': 1, 'econ_type': 'closed', 'comp_var': 'series'
-            },
-        'econ_open': {
-            'nr_int_heatex': 0, 'econ_type': 'open', 'comp_var': 'series'
-            },
-        'econ_open_ihx': {
-            'nr_int_heatex': 1, 'econ_type': 'open', 'comp_var': 'series'
-            },
-        'ihx_econ_open': {
-            'nr_int_heatex': 1, 'econ_type': 'open', 'comp_var': 'series'
-            },
-        'pc_econ_closed': {
-            'nr_int_heatex': 0, 'econ_type': 'closed', 'comp_var': 'parallel'
-            },
-        'pc_econ_closed_ihx': {
-            'nr_int_heatex': 1, 'econ_type': 'closed', 'comp_var': 'parallel'
-            },
-        'pc_econ_closed_2ihx': {
-            'nr_int_heatex': 2, 'econ_type': 'closed', 'comp_var': 'parallel'
-            },
-        'pc_econ_open': {
-            'nr_int_heatex': 0, 'econ_type': 'open', 'comp_var': 'parallel'
-            },
-        'pc_econ_open_ihx': {
-            'nr_int_heatex': 1, 'econ_type': 'open', 'comp_var': 'parallel'
-            },
-        'pc_econ_open_2ihx': {
-            'nr_int_heatex': 2, 'econ_type': 'open', 'comp_var': 'parallel'
-            }
+    'ic': {
+        'base_topology': 'Zwischenkühlung',
+        'display_name': 'Allgemein',
+        'nr_ihx': 0,
+        'econ_type': None,
+        'comp_var': 'series',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
         },
-    'Flashtank': {
-        'flash': {'nr_int_heatex': 0}
+    'econ_closed': {
+        'base_topology': 'Economizer',
+        'display_name': 'Geschlossen | Reihenschaltung',
+        'nr_ihx': 0,
+        'econ_type': 'closed',
+        'comp_var': 'series',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
         },
-    'Kaskadierter Kreis': {
-        'cascade': {'nr_int_heatex': 0},
-        'cascade_2ihx': {'nr_int_heatex': 2}
+    'econ_closed_ihx': {
+        'base_topology': 'Economizer',
+        'display_name': 'Geschlossen | Reihenschaltung | interne WÜT (Variante A)',
+        'nr_ihx': 1,
+        'econ_type': 'closed',
+        'comp_var': 'series',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'ihx_econ_closed': {
+        'base_topology': 'Economizer',
+        'display_name': 'Geschlossen | Reihenschaltung | interne WÜT (Variante B)',
+        'nr_ihx': 1,
+        'econ_type': 'closed',
+        'comp_var': 'series',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'econ_open': {
+        'base_topology': 'Economizer',
+        'display_name': 'Offen | Reihenschaltung',
+        'nr_ihx': 0,
+        'econ_type': 'open',
+        'comp_var': 'series',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'econ_open_ihx': {
+        'base_topology': 'Economizer',
+        'display_name': 'Offen | Reihenschaltung | interne WÜT (Variante A)',
+        'nr_ihx': 1,
+        'econ_type': 'open',
+        'comp_var': 'series',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'ihx_econ_open': {
+        'base_topology': 'Economizer',
+        'display_name': 'Offen | Reihenschaltung | interne WÜT (Variante B)',
+        'nr_ihx': 1,
+        'econ_type': 'open',
+        'comp_var': 'series',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'pc_econ_closed': {
+        'base_topology': 'Economizer',
+        'display_name': 'Geschlossen | Parallelschaltung',
+        'nr_ihx': 0,
+        'econ_type': 'closed',
+        'comp_var': 'parallel',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'pc_econ_closed_ihx': {
+        'base_topology': 'Economizer',
+        'display_name': 'Geschlossen | Parallelschaltung | interne WÜT',
+        'nr_ihx': 1,
+        'econ_type': 'closed',
+        'comp_var': 'parallel',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'pc_econ_closed_2ihx': {
+        'base_topology': 'Economizer',
+        'display_name': 'Geschlossen | Parallelschaltung | doppelte interne WÜT',
+        'nr_ihx': 2,
+        'econ_type': 'closed',
+        'comp_var': 'parallel',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'pc_econ_open': {
+        'base_topology': 'Economizer',
+        'display_name': 'Offen | Parallelschaltung',
+        'nr_ihx': 0,
+        'econ_type': 'open',
+        'comp_var': 'parallel',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'pc_econ_open_ihx': {
+        'base_topology': 'Economizer',
+        'display_name': 'Offen | Parallelschaltung | interne WÜT',
+        'nr_ihx': 1,
+        'econ_type': 'open',
+        'comp_var': 'parallel',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'pc_econ_open_2ihx': {
+        'base_topology': 'Economizer',
+        'display_name': 'Offen | Parallelschaltung | doppelte interne WÜT',
+        'nr_ihx': 2,
+        'econ_type': 'open',
+        'comp_var': 'parallel',
+        'nr_cycles': 1,
+        'process_type': 'subcritical'
+        },
+    'flash': {
+        'base_topology': 'Flashtank',
+        'display_name': 'Allgemein',
+        'nr_ihx': 0,
+        'econ_type': None,
+        'comp_var': 'series',
+        'nr_cycles': 2,
+        'process_type': 'subcritical'
+        },
+    'cascade': {
+        'base_topology': 'Kaskadierter Kreis',
+        'display_name': 'Allgemein',
+        'nr_ihx': 0,
+        'econ_type': None,
+        'comp_var': 'series',
+        'nr_cycles': 2,
+        'process_type': 'subcritical'
+        },
+    'cascade_2ihx': {
+        'base_topology': 'Kaskadierter Kreis',
+        'display_name': 'Doppelte interne WÜT',
+        'nr_ihx': 2,
+        'econ_type': None,
+        'comp_var': 'series',
+        'nr_cycles': 2,
+        'process_type': 'subcritical'
         }
     }
 
+# %% Misc
+econ_translate = {
+    'offen': 'open',
+    'geschlossen': 'closed'
+}
+comp_translate = {
+    'Reihenschaltung': 'series',
+    'Parallelschaltung': 'parallel'
+}
+
+# %% Styling
+st_color_hex = '#ff4b4b'
