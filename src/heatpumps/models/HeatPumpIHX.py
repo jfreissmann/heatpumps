1b0d5ec5b8f225d35c0311327ff77cc7a6548c0a
bilwint <thomas.bilwin4@gmail.com>
2024-04-30 18:06:50 +0200

    part load exergy plot HP_IHX

--- src/heatpumps/models/HeatPumpIHX.py
+++ src/heatpumps/models/HeatPumpIHX.py
@@ -270,7 +270,7 @@ class HeatPumpIHX(HeatPumpBase):
             )
 
         results_offdesign = pd.DataFrame(
-            index=multiindex, columns=['Q', 'P', 'COP', 'residual']
+            index=multiindex, columns=['Q', 'P', 'COP', 'epsilon', 'residual']
             )
 
         for T_hs_ff in self.T_hs_ff_stablerange:
@@ -306,6 +306,7 @@ class HeatPumpIHX(HeatPumpBase):
                         self.nw.solve(
                             'offdesign', design_path=self.design_path
                             )
+                        self.perform_exergy_analysis()
                         failed = False
                     except ValueError:
                         failed = True
@@ -362,6 +363,7 @@ class HeatPumpIHX(HeatPumpBase):
                             if failed:
                                 results_offdesign.loc[idx, 'Q'] = np.nan
                                 results_offdesign.loc[idx, 'P'] = np.nan
+                                results_offdesign.loc[idx, 'epsilon'] = np.nan
                             else:
                                 results_offdesign.loc[idx, 'Q'] = abs(
                                     self.buses['heat output'].P.val * 1e-6
@@ -369,6 +371,9 @@ class HeatPumpIHX(HeatPumpBase):
                                 results_offdesign.loc[idx, 'P'] = (
                                     self.buses['power input'].P.val * 1e-6
                                     )
+                                results_offdesign.loc[idx, 'epsilon'] = round(
+                                    self.ean.network_data['epsilon'], 3
+                                )
 
                             results_offdesign.loc[idx, 'COP'] = (
                                 results_offdesign.loc[idx, 'Q']
