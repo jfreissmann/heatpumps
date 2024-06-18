9c67933b7377f1d803dd1d8a8257489844f1aeb6
Jonas Freißmann <j.freissmann@web.de>
2024-06-18 11:51:10 +0200

    Simplify imports of heat pump model classes.

--- src/heatpumps/models/__init__.py
+++ src/heatpumps/models/__init__.py
@@ -1,4 +1,38 @@
-from models import (HeatPumpCascade, HeatPumpCascade2IHX, HeatPumpEcon,
-                    HeatPumpEconIHX, HeatPumpFlash, HeatPumpIC, HeatPumpIHX,
-                    HeatPumpIHXEcon, HeatPumpIHXPC, HeatPumpIHXPCIHX,
-                    HeatPumpPC, HeatPumpPCIHX, HeatPumpSimple)
+from .HeatPumpBase import HeatPumpBase
+from .HeatPumpCascade import HeatPumpCascade
+from .HeatPumpCascade2IHX import HeatPumpCascade2IHX
+from .HeatPumpCascade2IHXTrans import HeatPumpCascade2IHXTrans
+from .HeatPumpCascadeTrans import HeatPumpCascadeTrans
+from .HeatPumpEcon import HeatPumpEcon
+from .HeatPumpEconIHX import HeatPumpEconIHX
+from .HeatPumpEconIHXTrans import HeatPumpEconIHXTrans
+from .HeatPumpEconTrans import HeatPumpEconTrans
+from .HeatPumpFlash import HeatPumpFlash
+from .HeatPumpFlashTrans import HeatPumpFlashTrans
+from .HeatPumpIC import HeatPumpIC
+from .HeatPumpICTrans import HeatPumpICTrans
+from .HeatPumpIHX import HeatPumpIHX
+from .HeatPumpIHXEcon import HeatPumpIHXEcon
+from .HeatPumpIHXEconTrans import HeatPumpIHXEconTrans
+from .HeatPumpIHXPC import HeatPumpIHXPC
+from .HeatPumpIHXPCIHX import HeatPumpIHXPCIHX
+from .HeatPumpIHXPCIHXTrans import HeatPumpIHXPCIHXTrans
+from .HeatPumpIHXPCTrans import HeatPumpIHXPCTrans
+from .HeatPumpIHXTrans import HeatPumpIHXTrans
+from .HeatPumpPC import HeatPumpPC
+from .HeatPumpPCIHX import HeatPumpPCIHX
+from .HeatPumpPCIHXTrans import HeatPumpPCIHXTrans
+from .HeatPumpPCTrans import HeatPumpPCTrans
+from .HeatPumpSimple import HeatPumpSimple
+from .HeatPumpSimpleTrans import HeatPumpSimpleTrans
+
+__all__ = [
+    'HeatPumpBase', 'HeatPumpCascade', 'HeatPumpCascade2IHX',
+    'HeatPumpCascade2IHXTrans', 'HeatPumpCascadeTrans', 'HeatPumpEcon',
+    'HeatPumpEconIHX', 'HeatPumpEconIHXTrans', 'HeatPumpEconTrans',
+    'HeatPumpFlash', 'HeatPumpFlashTrans', 'HeatPumpIC', 'HeatPumpICTrans',
+    'HeatPumpIHX', 'HeatPumpIHXEcon', 'HeatPumpIHXEconTrans', 'HeatPumpIHXPC',
+    'HeatPumpIHXPCIHX', 'HeatPumpIHXPCIHXTrans', 'HeatPumpIHXPCTrans',
+    'HeatPumpIHXTrans', 'HeatPumpPC', 'HeatPumpPCIHX', 'HeatPumpPCIHXTrans',
+    'HeatPumpPCTrans', 'HeatPumpSimple', 'HeatPumpSimpleTrans'
+]
