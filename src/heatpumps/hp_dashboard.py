eef7f224aef5f22015f2d5eb6bdeaec004f83a43
Jonas Freißmann <j.freissmann@web.de>
2024-06-18 13:31:42 +0200

    Change hp_dashboard.py to allow run from package command.

--- src/heatpumps/hp_dashboard.py
+++ src/heatpumps/hp_dashboard.py
@@ -44,7 +44,7 @@ def info_df(label, refrigs):
 
     return df_refrig
 
-src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
 
 # %% MARK: Initialisation
 refrigpath = os.path.join(src_path, 'refrigerants.json')
@@ -530,13 +530,33 @@ if mode == 'Start':
             )
 
     with st.expander('Copyright'):
-        licpath = os.path.abspath(os.path.join(
-            os.path.dirname(__file__), '..', '..', 'LICENSE'
-            ))
-        with open(licpath, 'r', encoding='utf-8') as file:
-            lictext = file.read()
 
-        st.success('#### Softwarelizenz\n' + lictext.replace('(c)', '©'))
+        st.success(
+            """
+            #### Softwarelizenz
+            MIT License
+
+            Copyright © 2023 Jonas Freißmann and Malte Fritz
+
+            Permission is hereby granted, free of charge, to any person obtaining a copy
+            of this software and associated documentation files (the "Software"), to deal
+            in the Software without restriction, including without limitation the rights
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            copies of the Software, and to permit persons to whom the Software is
+            furnished to do so, subject to the following conditions:
+
+            The above copyright notice and this permission notice shall be included in all
+            copies or substantial portions of the Software.
+
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.
+            """
+        )
 
 if mode == 'Auslegung':
     # %% MARK: Design Simulation
