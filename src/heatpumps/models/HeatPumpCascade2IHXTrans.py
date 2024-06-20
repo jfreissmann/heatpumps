06a2169329aad2289e84a5751165e3e065ef8145
bilwint <thomas.bilwin4@gmail.com>
2024-06-20 18:32:58 +0200

    C0 to C1 in models for better solvability at high sink temperature

--- src/heatpumps/models/HeatPumpCascade2IHXTrans.py
+++ src/heatpumps/models/HeatPumpCascade2IHXTrans.py
@@ -258,7 +258,7 @@ class HeatPumpCascade2IHXTrans(HeatPumpBase):
             T=self.params['C3']['T'], p=self.params['C3']['p'],
             fluid={self.si: 1}
         )
-        self.conns['C0'].set_attr(T=self.params['C0']['T'])
+        self.conns['C1'].set_attr(T=self.params['C1']['T'])
 
         # Perform initial simulation and unset starting values
         self._solve_model(**kwargs)
@@ -517,7 +517,7 @@ class HeatPumpCascade2IHXTrans(HeatPumpBase):
         ) * 1e-5
         h_trans_out = PSI(
             'H', 'P', self.params['A0']['p'] * 1e5,
-            'T', self.params['C0']['T'] + self.params['trans']['ttd_l'] + 273.15,
+            'T', self.params['C1']['T'] + self.params['trans']['ttd_l'] + 273.15,
             self.wf2
         ) * 1e-3
 
