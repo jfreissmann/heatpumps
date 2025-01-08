fa4798a2c9a579f63964e35c300703e5c2dad48c
Jonas Freißmann <j.freissmann@web.de>
2025-01-08 14:12:55 +0100

    Consolidate generate_state_diagram method into HeatPumpCascadeBase.

--- src/heatpumps/models/HeatPumpCascadeEcon.py
+++ src/heatpumps/models/HeatPumpCascadeEcon.py
@@ -488,55 +488,6 @@ class HeatPumpCascadeEcon(HeatPumpCascadeBase):
 
         return data
 
-    def generate_state_diagram(self, refrig='', diagram_type='logph',
-                               style='light', figsize=(16, 10),
-                               legend=True, legend_loc='upper left',
-                               return_diagram=False, savefig=True,
-                               open_file=True, **kwargs):
-        kwargs1 = {}
-        kwargs2 = {}
-        if 'xlims' in kwargs:
-            kwargs1['xlims'] = kwargs['xlims'][0]
-            kwargs2['xlims'] = kwargs['xlims'][1]
-        if 'ylims' in kwargs:
-            kwargs1['ylims'] = kwargs['ylims'][0]
-            kwargs2['ylims'] = kwargs['ylims'][1]
-        if return_diagram:
-            diagram1 = super().generate_state_diagram(
-                refrig=self.params['setup']['refrig1'],
-                style=style, figsize=figsize,
-                diagram_type=diagram_type, legend=legend,
-                legend_loc=legend_loc,
-                return_diagram=return_diagram, savefig=savefig,
-                open_file=open_file, cycle=1, **kwargs1
-                )
-            diagram2 = super().generate_state_diagram(
-                refrig=self.params['setup']['refrig2'],
-                style=style, figsize=figsize,
-                diagram_type=diagram_type, legend=legend,
-                legend_loc=legend_loc,
-                return_diagram=return_diagram, savefig=savefig,
-                open_file=open_file, cycle=2, **kwargs2
-                )
-            return diagram1, diagram2
-        else:
-            super().generate_state_diagram(
-                refrig=self.params['setup']['refrig1'],
-                style=style, figsize=figsize,
-                diagram_type=diagram_type, legend=legend,
-                legend_loc=legend_loc,
-                return_diagram=return_diagram, savefig=savefig,
-                open_file=open_file, cycle=1, **kwargs1
-                )
-            super().generate_state_diagram(
-                refrig=self.params['setup']['refrig2'],
-                style=style, figsize=figsize,
-                diagram_type=diagram_type, legend=legend,
-                legend_loc=legend_loc,
-                return_diagram=return_diagram, savefig=savefig,
-                open_file=open_file, cycle=2, **kwargs2
-                )
-
     def check_consistency(self):
         """Perform all necessary checks to protect consistency of parameters."""
         self.check_expansion_into_vapor_liquid_region(
