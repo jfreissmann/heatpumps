61650a125abc7ddedeeb130a5d6e7eca412d5b89
Jonas Freißmann <j.freissmann@web.de>
2025-12-10 12:09:02 +0100

    Add publications section to landing page.

--- src/heatpumps/hp_dashboard.py
+++ src/heatpumps/hp_dashboard.py
@@ -685,6 +685,26 @@ if mode == 'Start':
             """
             )
 
+    with st.expander('Publikationen'):
+        st.success(
+            """
+
+            [1] Malte Fritz, Jonas Freißmann & Ilja Tuschy. Open-Source Web 
+            Dashboard zur Simulation, Analyse und Bewertung von Wärmepumpen. 
+            Tagungsband (2. Version) - 2. Konferenz der Norddeutschen 
+            Wärmeforschung: Gemeinsam die Wärmewende voranbringen., 
+            S. 24-33. HafenCity Universität, 2025. 
+            [doi:10.34712/142.68](https://doi.org/10.34712/142.68).
+
+            [2] Jonas Freißmann, Malte Fritz & Ilja Tuschy. 
+            Hochtemperaturwärmepumpen in der Nah- und Fernwärmeversorgung - 
+            Technologieperspektive für den kurz- und mittelfristigen Einsatz 
+            in multivalenten Systemen. Abschlussbericht, Zentrum für 
+            nachhaltige Energiesysteme, 2024. 
+            [doi:10.5281/zenodo.13311533](https://doi.org/10.5281/zenodo.13311533).
+            """
+            )
+
     with st.expander('Disclaimer'):
         st.warning(
             """
